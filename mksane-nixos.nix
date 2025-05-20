5fdc5c32b65040b5732a50b9693f746e6a754970
shnCanos <catellacatellacarettacaretta@gmail.com>
2025-05-20 11:20:41 +0100

    made bad fix

--- mksane-nixos.nix
+++ mksane-nixos.nix
@@ -6,65 +6,70 @@ in
 	options = {
 		programs.mkSane = {
 			enable = lib.mkEnableOption "Whether to enable mksane";
-			libWorkarounds = lib.mkEnableOption "Whether to enable workarounds for libraries";
-			copyLib = lib.mkEnableOption "Whether to copy /run/current-system/sw/lib to /lib and /usr/lib";
-			copyBin = lib.mkEnableOption "Whether to copy /run/current-system/sw/bin to /bin and /usr/bin";
-			copysBin = lib.mkEnableOption "Whether to copy /run/current-system/sw/sbin to /sbin and /usr/sbin";
-			fontWorkarounds = lib.mkEnableOption "Whether to enable workarounds for fonts";
+			copyPaths = lib.mkEnableOption "Whether to copy some paths from /run/current-system/sw to their expected places";
 		};
 	};
 	
 	config = 
 		let libraries = (import ./libraries.nix) pkgs;
-
-		mkRoSymBind = path: {
-			device = path;
-			fsType = "fuse.bindfs";
-			options = [ "ro" "resolve-symlinks" "x-gvfs-hide" ];
-		};
 		in
 			lib.mkIf (cfg.enable) {
-				system.fsPackages = [ pkgs.bindfs ];
-				fileSystems = 
-					let 
-						aggregate = paths: pkgs.buildEnv {
-							name = "system-packages";
-							paths = config.environment.systemPackages;
-							pathsToLink = paths;
-						};
-						bins = aggregate ["/bin"];
-						libs = aggregate ["/lib"];
-						sbins = aggregate ["/sbin"];
+			system.fsPackages = [ pkgs.bindfs ];
+			fileSystems =
+				let
+					mkRoSymBind = path: {
+						device = path;
+						fsType = "fuse.bindfs";
+						options = [ "ro" "resolve-symlinks" "x-gvfs-hide" ];
+					};
+					aggregatedIcons = pkgs.buildEnv {
+						name = "system-icons";
+						paths = with pkgs; [
+							libsForQt5.breeze-qt5 # for plasma
+							# gnome.gnome-themes-extra
+						];
+						pathsToLink = [ "/share/icons" ];
+					};
+					aggregatedFonts = pkgs.buildEnv {
+						name = "system-fonts";
+						paths = config.fonts.packages;
+						pathsToLink = [ "/share/fonts" ];
+					};
+				in
+				{
+					"/usr/share/icons" = mkRoSymBind "${aggregatedIcons}/share/icons";
+					"/usr/local/share/fonts" = mkRoSymBind "${aggregatedFonts}/share/fonts";
+				};
 
-						aggregatedIcons = pkgs.buildEnv {
-							name = "system-icons";
-							paths = with pkgs; [
-								libsForQt5.breeze-qt5 # for plasma
-							];
-							pathsToLink = [ "/share/icons" ];
-						};
-						aggregatedFonts = pkgs.buildEnv {
-							name = "system-fonts";
-							paths = config.fonts.packages;
-							pathsToLink = [ "/share/fonts" ];
-						};
 
-						fs = {
-							"/bin" = mkRoSymBind "${bins}/bin";
-							"/usr/bin" = mkRoSymBind "${bins}/bin";
-							"/lib" = mkRoSymBind "${libs}/lib";
-							"/usr/lib" = mkRoSymBind "${libs}/lib";
-							"/sbin" = mkRoSymBind "${sbins}/sbin";
-							"/usr/sbin" = mkRoSymBind "${sbins}/sbin";
-							"/usr/share/icons" = mkRoSymBind "${aggregatedIcons}/share/icons";
-							"/usr/local/share/fonts" = mkRoSymBind "${aggregatedFonts}/share/fonts";
-						};
-					in fs;
-
+				system.activationScripts = lib.mkIf cfg.copyPaths {
+					copyBin = {
+						text = ''
+											# Binary packages
+											${pkgs.rsync}/bin/rsync -a --ignore-existing /run/current-system/sw/bin /
+											${pkgs.rsync}/bin/rsync -a --ignore-existing /run/current-system/sw/bin /usr/
+										'';
+					};
+					copyLib = {
+						text = ''
+											# Libraries (done by nix-ld, but anyway)
+											${pkgs.rsync}/bin/rsync -a --ignore-existing /run/current-system/sw/lib /
+											${pkgs.rsync}/bin/rsync -a --ignore-existing /run/current-system/sw/lib /usr/
+										'';
+					};
+					copySbin = {
+						text = ''
+											# sbin
+											${pkgs.rsync}/bin/rsync -a --ignore-existing /run/current-system/sw/sbin /
+											${pkgs.rsync}/bin/rsync -a --ignore-existing /run/current-system/sw/sbin /usr/
+										'';
+					};
+				};
 				programs.nix-ld.enable = true;
 				programs.nix-ld.libraries = libraries;
 				environment.systemPackages = config.programs.nix-ld.libraries;
 				environment.variables = {
+					# For some reason nix-ld doesn't add all of them so I add this path
 					LD_LIBRARY_PATH = lib.mkForce "/run/current-system/sw/lib";
 					PKG_CONFIG_PATH = "/run/current-system/sw/lib/pkgconfig";
 		};};
